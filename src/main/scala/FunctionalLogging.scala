b4aca3751f00c69416773a5cad187ca93499f7c9
rizwan <rizwan@rizwans-MBP.fios-router.home>
2024-01-31 15:29:33 -0500

    more functional logging example with 2 function calls piped together with a log in between

--- src/main/scala/FunctionalLogging.scala
+++ src/main/scala/FunctionalLogging.scala
@@ -7,6 +7,9 @@
       if id < 10 then (None, s"No user found for id=$id")
       else (Some(User(id, "Rizwan")), s"Found user for id=$id")
 
+
+  val capitalizeUserName: User => String = _.name.toUpperCase
+
   def log[A]: PartialFunction[(A, String), (A, String)] =
     case t =>
       if t._2.nonEmpty then println(s"${t._2}")
@@ -29,3 +32,6 @@
 
   val thirdCall = getUserById andThen removeLogContext // won't log and remove the log from the tuple.
   println(s"thirdCall:${thirdCall(25)}")
+
+  val fourthCall = (getUserById andThen logAndRemoveLog andThen(_.map(capitalizeUserName)))(25) // calls 2 functions in a pipeline and logs the output of the first function as well.
+  println(s"fourthCall:$fourthCall")
