1d4d95897f3170e14f064896d3e2aa4e4cb5f8b5
rizwan <rizwan@rizwans-MBP.fios-router.home>
2024-01-31 14:51:52 -0500

    Adding functional logging pattern with examples

---
+++ src/main/scala/FunctionalLogging.scala
@@ -0,0 +1,31 @@
+@main def functionalLogging(): Unit =
+
+  case class User(id: Int, name: String)
+
+  val getUserById: Int => (Option[User], String) =
+    id =>
+      if id < 10 then (None, s"No user found for id=$id")
+      else (Some(User(id, "Rizwan")), s"Found user for id=$id")
+
+  def log[A]: PartialFunction[(A, String), (A, String)] =
+    case t =>
+      if t._2.nonEmpty then println(s"${t._2}")
+      t
+
+  def logAndRemoveLog[A]: PartialFunction[(A, String), A] =
+    case tuple if tuple._2.nonEmpty =>
+      println(s"${tuple._2}")
+      tuple._1
+    case rest => rest._1
+
+  def removeLogContext[A]: PartialFunction[(A, String), A] = _._1
+
+
+  val firstCall = (getUserById andThen logAndRemoveLog)(5) // logs and removes the log string from the tuple.
+  println(s"firstCall:$firstCall")
+
+  val secondCall = (getUserById andThen log)(15) // logs and keeps the log string in the tuple.
+  println(s"secondCall:$secondCall")
+
+  val thirdCall = getUserById andThen removeLogContext // won't log and remove the log from the tuple.
+  println(s"thirdCall:${thirdCall(25)}")
